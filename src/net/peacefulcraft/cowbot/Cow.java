7264bde0a48fa1249d39db8383597de97964086a
Parsonswy <parsonswy@gmail.com>
2020-05-12 19:23:32 -0400

    !list command

--- src/net/peacefulcraft/cowbot/Cow.java
+++ src/net/peacefulcraft/cowbot/Cow.java
@@ -1,5 +1,7 @@
 package net.peacefulcraft.cowbot;
 
+import java.util.ArrayList;
+
 import discord4j.core.DiscordClient;
 import discord4j.core.DiscordClientBuilder;
 import discord4j.core.event.domain.guild.GuildCreateEvent;
@@ -11,6 +13,7 @@ import discord4j.core.object.presence.Presence;
 import net.peacefulcraft.cowbot.handlers.DisconnectEventHandler;
 import net.peacefulcraft.cowbot.handlers.GameChatMessageHandler;
 import net.peacefulcraft.cowbot.handlers.GuildCreateEventHandler;
+import net.peacefulcraft.cowbot.handlers.ListCommandHandler;
 import net.peacefulcraft.cowbot.handlers.ModChatMessageHandler;
 import net.peacefulcraft.cowbot.handlers.PingMessageHandler;
 import net.peacefulcraft.cowbot.handlers.ReadyEventHandler;
@@ -22,9 +25,16 @@ public class Cow implements Runnable{
   private boolean active = true;
     public boolean isActive() { return active; }
 
+  private ArrayList<String> subscribedChannels;
+
   public Cow(String token) {
     bot = DiscordClientBuilder.create(token).build();
     bot.updatePresence(Presence.online(Activity.listening("your conversations")));
+
+    subscribedChannels = new ArrayList<String>();
+    subscribedChannels.add(CowBot.getConfig().getCCChannelId());
+    subscribedChannels.add(CowBot.getConfig().getGamechatChannelId());
+    subscribedChannels.add(CowBot.getConfig().getStaffchatChannelId());
 
     // Setup stream consumers
     bot.getEventDispatcher().on(ReadyEvent.class)
@@ -45,15 +55,33 @@ public class Cow implements Runnable{
 
     bot.getEventDispatcher().on(MessageCreateEvent.class)
       .map(MessageCreateEvent::getMessage)
+      .filter(message -> subscribedChannels.contains(message.getChannelId().asString()))
+      .filter(message -> message.getContent().orElse("").equalsIgnoreCase("!list"))
+      .subscribe(ListCommandHandler::handle);
+
+    bot.getEventDispatcher().on(MessageCreateEvent.class)
+      .map(MessageCreateEvent::getMessage)
       .filter(message -> message.getChannelId().asString().equalsIgnoreCase(CowBot.getConfig().getGamechatChannelId()))
       .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+      .filter(message -> !isCommand(message.getContent().orElse("")))
       .subscribe(GameChatMessageHandler::handle);
 
     bot.getEventDispatcher().on(MessageCreateEvent.class)
       .map(MessageCreateEvent::getMessage)
       .filter(message -> message.getChannelId().asString().equalsIgnoreCase(CowBot.getConfig().getStaffchatChannelId()))
       .filter(message -> message.getAuthor().map(user -> !user.isBot()).orElse(false))
+      .filter(message -> !isCommand(message.getContent().orElse("")))
       .subscribe(ModChatMessageHandler::handle);
+  }
+
+  public static boolean isCommand(String content) {
+    if (content.length() > 1) {
+      if (content.charAt(0) == '!') {
+        return true;
+      }
+    }
+
+    return false;
   }
 
   @Override
---
+++ src/net/peacefulcraft/cowbot/handlers/ListCommandHandler.java
@@ -0,0 +1,55 @@
+package net.peacefulcraft.cowbot.handlers;
+
+import java.awt.Color;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Map.Entry;
+
+import discord4j.core.object.entity.Message;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
+import net.peacefulcraft.cowbot.CowBot;
+
+public class ListCommandHandler {
+  public static void handle(Message message) {
+    Set<String> servers = CowBot.getInstance().getProxy().getServers().keySet();
+    HashMap<String, ArrayList<String>> counts = new HashMap<String, ArrayList<String>>();
+    servers.forEach(server -> { counts.put(server, new ArrayList<String>()); });
+
+    for(ProxiedPlayer player : CowBot.getInstance().getProxy().getPlayers()) {
+      counts.get(player.getServer().getInfo().getName()).add(player.getName());
+    }
+    CowBot.logError("ready for embed");
+    CowBot.runAsync(
+      () -> {
+        CowBot.logError("generating embed");
+        message.getChannel().block().createMessage(messageSpec -> messageSpec.setEmbed(embed -> {
+          embed.setColor(new Color(169, 220, 169));
+          embed.setAuthor("PeacefulCraft Network Status", "https://status.peacefulcraft.net", "https://www.peacefulcraft.net/assets/logo-aglqhi2l.png");
+          
+          long graphEnd = ZonedDateTime.now().toInstant().toEpochMilli() - 60000;
+          long graphStart = graphEnd - 3600000;
+          embed.setImage("https://status.peacefulcraft.net/render/d-solo/MSFaPWjZz/peacefulcraft-network-player-counts?orgId=1&panelId=4&from=" + graphStart + "&to=" + graphEnd);
+
+          embed.setTimestamp(Instant.now());
+          embed.setFooter("Network Player Count - Last Hour", "");
+      
+          for(Entry<String, ArrayList<String>> list : counts.entrySet()) {
+            String userList = "**(" + list.getValue().size() + ")**";
+            for(String username : list.getValue()) {
+              userList += " " + username + ",";
+            }
+
+            if (list.getValue().size() > 0){
+              userList = userList.substring(0, userList.length() - 1);
+            }
+
+            embed.addField(list.getKey(), userList, false);
+          }
+        })).block();
+      }
+    );
+  }
+}
