0b2245a6a2e159beabede60196d28637b023a956
Parsonswy <parsonswy@gmail.com>
2020-05-20 23:54:56 -0400

    Pull Discord ranks in messages

--- src/net/peacefulcraft/cowbot/handlers/GameChatMessageHandler.java
+++ src/net/peacefulcraft/cowbot/handlers/GameChatMessageHandler.java
@@ -1,11 +1,13 @@
 package net.peacefulcraft.cowbot.handlers;
 
+import discord4j.core.object.entity.Member;
 import discord4j.core.object.entity.Message;
 import net.md_5.bungee.api.ChatColor;
 import net.md_5.bungee.api.chat.BaseComponent;
 import net.md_5.bungee.api.chat.ComponentBuilder;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.peacefulcraft.cowbot.CowBot;
+import net.peacefulcraft.cowbot.translation.ColorTranslator;
 
 public class GameChatMessageHandler {
 
@@ -13,12 +15,19 @@ public class GameChatMessageHandler {
     if (!message.getAuthor().isPresent()) { return; }
     String author = message.getAuthor().get().getUsername();;
 
+    Member sender = message.getAuthor().get().asMember(message.getGuild().block().getId()).block();
+    ChatColor senderColor = ColorTranslator.ColorToColorCode(sender.getColor().block());
+    String senderRank = sender.getRoles().blockLast().getName();
+
     if (!message.getContent().isPresent()) { return; }
     String content = message.getContent().get();
+
 
     BaseComponent[] formattedMessage = new ComponentBuilder()
       .append("[").color(ChatColor.GREEN)
       .append("Discord").color(ChatColor.GOLD)
+      .append("][").color(ChatColor.GREEN)
+      .append(senderRank).color(senderColor)
       .append("]").color(ChatColor.GREEN)
       .append(author + ": ").color(ChatColor.GRAY)
       .append(content).color(ChatColor.WHITE).create();
@@ -26,6 +35,8 @@ public class GameChatMessageHandler {
     CowBot.logMessage(
       ChatColor.GREEN + " [" +
       ChatColor.GOLD + "Discord" +
+      ChatColor.GREEN + "][" +
+      senderColor + senderRank +
       ChatColor.GREEN + "]" + 
       ChatColor.GRAY + author + ": " + 
       ChatColor.WHITE + content
