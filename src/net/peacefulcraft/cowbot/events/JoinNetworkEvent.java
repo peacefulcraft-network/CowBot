59dc004c55c95c7f88c6f3ecd8465ccee4742e5e
Parsonswy <parsonswy@gmail.com>
2020-11-15 16:03:06 -0500

    Try respecting silent join

--- src/net/peacefulcraft/cowbot/events/JoinNetworkEvent.java
+++ src/net/peacefulcraft/cowbot/events/JoinNetworkEvent.java
@@ -2,6 +2,7 @@ package net.peacefulcraft.cowbot.events;
 
 import discord4j.core.object.entity.TextChannel;
 import discord4j.core.object.util.Snowflake;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PostLoginEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -11,6 +12,10 @@ public class JoinNetworkEvent implements Listener{
   @EventHandler
   public void joinGameEvent(PostLoginEvent event) {
     if (CowBot.getConfig().getGamechatChannelId() == null || CowBot.getConfig().getGamechatChannelId().length() < 1) { return; }
+    
+    ProxiedPlayer p = event.getPlayer();
+    CowBot.logMessage("Player " + p.getName() + " permission fetch result " + p.hasPermission("multichat.staff.silentjoin"));
+    if (p.hasPermission("multichat.staff.silentjoin")) { return; }
 
     String message = "*" + event.getPlayer().getName() + " joined the network*";
     CowBot.runAsync(
--- src/net/peacefulcraft/cowbot/events/LeaveNetworkEvent.java
+++ src/net/peacefulcraft/cowbot/events/LeaveNetworkEvent.java
@@ -2,6 +2,7 @@ package net.peacefulcraft.cowbot.events;
 
 import discord4j.core.object.entity.TextChannel;
 import discord4j.core.object.util.Snowflake;
+import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.md_5.bungee.api.event.PlayerDisconnectEvent;
 import net.md_5.bungee.api.plugin.Listener;
 import net.md_5.bungee.event.EventHandler;
@@ -11,6 +12,10 @@ public class LeaveNetworkEvent implements Listener{
   @EventHandler
   public void leaveNetworkEvent(PlayerDisconnectEvent event) {
     if (CowBot.getConfig().getGamechatChannelId() == null || CowBot.getConfig().getGamechatChannelId().length() < 1) { return; }
+
+    ProxiedPlayer p = event.getPlayer();
+    CowBot.logMessage("Player " + p.getName() + " permission fetch result " + p.hasPermission("multichat.staff.silentjoin"));
+    if (p.hasPermission("multichat.staff.silentjoin")) { return; }
 
     String message = "*" + event.getPlayer().getName() + " has left the network*";
     CowBot.runAsync(
